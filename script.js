b810e3a35275d58812c105a5169207afec6a0c6a
felipepaloschii <felipe_paloschi@estudante.sesisenai.org.br>
2026-04-09 08:46:03 -0300

    add- 2if

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ function validaca() {
     let email = document.getElementById("email").value;
     let idade = document.getElementById("idade").value;
     let peso = document.getElementById("peso").value;
-    let tipo = document.querySelector('input[nome="tipo"]checked');
+    let tipo = document.querySelector('input[nome="tipo"]checked').value ;
     let telefone = document.getElementById("telefone").value;
     let cidade = document.getElementById("cidade").value;
     let estado = document.getElementById("estado").value;
 
+    if (!email.includes("0")) return alert ("Email Inválido");
+    if (idade < 16) return alert("Deve ser maior de 16 anos");
+    if (peso < 50) return alert("Você deve ter mais de 50kg");
 
-    if (idade < 16) return alert("Deve ser maior de 16 anos")
+    
 }
